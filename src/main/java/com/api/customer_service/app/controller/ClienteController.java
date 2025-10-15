453c3ea3fa4dea54a611ca785194bc301d4d0261
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2025-10-15 19:40:39 -0300

    Feat: Adicionando bibliotecas bases, configurações e profiles necessarios para projeto

---
+++ src/main/java/com/api/customer_service/app/controller/ClienteController.java
@@ -0,0 +1,8 @@
+package com.api.customer_service.app.controller;
+
+import com.api.customer_service.api.ClientesApi;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClienteController implements ClientesApi {
+}
---
+++ src/main/java/com/api/customer_service/app/controller/LoginController.java
@@ -0,0 +1,8 @@
+package com.api.customer_service.app.controller;
+
+import com.api.customer_service.api.LoginApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController implements LoginApi {
+}
