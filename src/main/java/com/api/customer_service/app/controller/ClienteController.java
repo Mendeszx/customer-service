f02b1f241899730927aad71f20669f24829c283a
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2025-10-21 21:11:38 -0300

    Feat: padronização de contrato com OpenApi em inglês

--- src/main/java/com/api/customer_service/app/controller/ClienteController.java
+++ src/main/java/com/api/customer_service/app/controller/ClienteController.java
@@ -1,8 +1,8 @@
 package com.api.customer_service.app.controller;
 
-import com.api.customer_service.api.ClientesApi;
+import com.api.customer_service.api.CustomersApi;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ClienteController implements ClientesApi {
+public class ClienteController implements CustomersApi {
 }
