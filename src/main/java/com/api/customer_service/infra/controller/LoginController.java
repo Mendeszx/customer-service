15f9e7c8b2ec78d2f79af132a31c59159031164f
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2025-10-21 23:16:19 -0300

    Feat: Update de funcionalidades da aplicação e de contrato

--- src/main/java/com/api/customer_service/infra/controller/LoginController.java
+++
@@ -1,8 +0,0 @@
-package com.api.customer_service.infra.controller;
-
-import com.api.customer_service.api.LoginApi;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class LoginController implements LoginApi {
-}
